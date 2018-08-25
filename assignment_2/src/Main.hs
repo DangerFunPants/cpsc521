a1276806f220fb47b2de1e267705e5f2f47d4476
Alexander James <alexander.james@ucalgary.ca>
2018-08-24 22:34:29 -0600

    Minor cleanup and formatting of output.

--- assignment_2/src/Main.hs
+++ assignment_2/src/Main.hs
@@ -5,7 +5,6 @@ import LexExprGrammar
 import AbsExprGrammar
 import qualified AST as A
 import ParExprGrammar
-import Text.Show.Pretty
 import ExprLifting
 import ExprPrinter
 import Data.Text.Prettyprint.Doc
@@ -29,17 +28,20 @@ main = do
     src <- getContents
     let (Ok ast) = runCompiler (lexSource src)
         (Right convd) = A.doConv ast
-        callGraph = cgProg ast
-        initSets = initSetProg ast
-        lifted = doFixedPoint initSets callGraph
-        (Right lifted') = (runIdentity . runExceptT) $ liftProg lifted ast
+        callGraph = cgProg convd
+        initSet = initSetProg convd
+        lifted = doFixedPoint initSet callGraph
+        lifted' = 
+            case (runIdentity . runExceptT) $ liftProg lifted convd of
+                (Left err) -> pretty err
+                (Right l) -> pretty l
         prettyAst = pretty ast
-        prettyLifted = pretty lifted'
-    putStrLn $ ppShow ast
-    putStrLn $ ppShow convd
-    putStrLn $ ppShow callGraph
-    putStrLn $ ppShow initSets
-    putStrLn $ ppShow lifted
-    putStrLn $ ppShow lifted'
+    putStrLn $ "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+    putStrLn $ "                      Original Source                         "
+    putStrLn $ ""
     putStrLn $ show prettyAst
-    putStrLn $ show prettyLifted
+    putStrLn $ "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+    putStrLn $ "                        Lifted Source                         "
+    putStrLn $ ""
+    putStrLn $ show lifted'
+    putStrLn $ "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
